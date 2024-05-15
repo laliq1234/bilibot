acd32aa1d7f87ba5865dddb05d422c0671c7d614
laliq1234 <72446026+laliq1234@users.noreply.github.com>
2024-05-15 08:10:11 +0200

    Update compress_model.py

--- tools/compress_model.py
+++ tools/compress_model.py
@@ -1,15 +1,1 @@
-from mlx_lm import convert
 
-# Compress Qwen1.5-32B-Chat.
-convert(
-    "models/Qwen1.5-32B-Chat",
-    mlx_path="models/Qwen1.5-32B-Chat-4Bit",
-    quantize=True
-)
-
-# Compress fine-tuned Qwen1.5-32B-Chat.
-# convert(
-#     "models/Qwen1.5-32B-Chat-FT", 
-#     mlx_path="models/Qwen1.5-32B-Chat-FT-4Bit",
-#     quantize=True
-# )
